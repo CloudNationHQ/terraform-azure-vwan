d6357ee9116bd94ec8117db81caa606dc2593ffd
haelekuin <haelekuin@catboys.cafe>
2025-06-06 14:28:27 +0000

    fix: remove vpn link validation condition (#112)
    * Fix: Adjust vpn_link validation condition
    
    Originally failed if another vhub without site_to_site_vpn config was
    configure
    
    * Use try and coalesce to handle both null and missing values
    
    * Remove s2s vpn validation entirely as plans fail regardless

--- variables.tf
+++ variables.tf
@@ -202,20 +202,6 @@ variable "vwan" {
     condition     = var.vwan.resource_group_name != null || var.resource_group_name != null
     error_message = "resource group name must be provided either in the object or as a separate variable."
   }
-
-  validation {
-    condition = can(flatten([
-      for vhub_key, vhub in var.vwan.vhubs : [
-        for site_key, site in vhub.site_to_site_vpn.vpn_sites : [
-          for conn_key, conn in site.connections : [
-            for conn_link_key, conn_link in conn.vpn_links :
-            regex("^${coalesce(site.vpn_links[conn_link_key].name, conn_link_key)}$", coalesce(conn_link.name, conn_link_key))
-          ]
-        ]
-      ]
-    ]))
-    error_message = "vpn_link names must match exactly between site and connection"
-  }
 }
 
 variable "naming" {
