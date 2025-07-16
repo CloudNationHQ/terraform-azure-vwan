0446dae2d97f432737414d26c72a626f5920d08e
dkooll <dennis.kool@cloudnation.nl>
2025-07-16 12:48:49 +0200

    fix: fix next hop validation routing intents

--- modules/routing-intent/variables.tf
+++ modules/routing-intent/variables.tf
@@ -35,7 +35,7 @@ variable "configs" {
   validation {
     condition = alltrue([
       for config_key, config in var.configs : alltrue([
-        for policy_key, policy in config.routing_policies : can(regex("^/subscriptions/[^/]+/resourceGroups/[^/]+/providers/Microsoft.Network/(azureFirewalls|networkVirtualAppliances)/[^/]+$", policy.next_hop))
+        for policy_key, policy in config.routing_policies : can(regex("^/subscriptions/[^/]+/resourceGroups/[^/]+/providers/(Microsoft\\.Network/(azureFirewalls|networkVirtualAppliances)|Microsoft\\.Solutions/applications)/[^/]+$", policy.next_hop))
       ])
     ])
     error_message = "All next_hop values must be valid Azure Firewall or Network Virtual Appliance resource IDs."
