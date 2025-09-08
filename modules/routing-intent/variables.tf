cfec9683398b6c354d0bb5295da6a4255867549b
dkooll <dennis.kool@cloudnation.nl>
2025-09-08 10:53:58 +0200

    fix: fix case sensitive issue in routing intent validation

--- modules/routing-intent/variables.tf
+++ modules/routing-intent/variables.tf
@@ -47,7 +47,7 @@ variable "configs" {
   validation {
     condition = alltrue([
       for config_key, config in var.configs : alltrue([
-        for policy_key, policy in config.routing_policies : can(regex("^/subscriptions/[^/]+/resourcegroups/[^/]+/providers/Microsoft\\.", policy.next_hop))
+        for policy_key, policy in config.routing_policies : can(regex("^/subscriptions/[^/]+/resource[Gg]roups/[^/]+/providers/Microsoft\\.", policy.next_hop))
       ])
     ])
     error_message = "All next_hop values must be valid Azure resource IDs."
